955fb9a26f28c0ba99fa39e7028665f8bc2e5a78
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2024-09-27 20:28:03 -0300

    Save exercise: celsius-to-fahrenheit

---
+++ python-exercises/celsius-to-fahrenheit/celsius-to-fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input('Enter the temperature in Celsius (ºC): '))
+
+fahrenheit = ((9 * celsius) / 5) + 32
+
+print('The value of %.2f°C is equivalent to %.2f°F.' % (celsius, fahrenheit))
---
+++ python-exercises/celsius-to-fahrenheit/exercicio-description.md
@@ -0,0 +1,5 @@
+# Exercise - Celsius to Fahrenheit
+
+Develop an algorithm that converts a temperature from Celsius (C) to Fahrenheit (F). The conversion equation is:
+
+![Formula](./formula.png)
