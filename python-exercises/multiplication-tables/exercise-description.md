06bdf92f1ddf6b882fbc0d2f2914c8428a8b635f
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2024-09-29 21:22:41 -0300

    Save exercise: multiplication-tables

---
+++ python-exercises/multiplication-tables/exercise-description.md
@@ -0,0 +1,3 @@
+# Exercise - Multiplication Tables
+
+Write an algorithm that prints the multiplication tables from 1 to 10 using a repetition loop.
---
+++ python-exercises/multiplication-tables/multiplication-tables.py
@@ -0,0 +1,9 @@
+i = 1
+
+while i <= 10 :
+    j = 1
+    while j <= 10 :
+        print(f'{i} * {j} = {i * j}')
+        j += 1
+    print('\n')
+    i += 1
