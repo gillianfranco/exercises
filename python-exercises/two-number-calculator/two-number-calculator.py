cf24eca3645b108a2174835443db43d7b1608cf3
Gillian Franco <gillian.melo.franco17@gmail.com>
2024-10-06 15:56:17 -0300

    Update exercise: two-number-calculator

--- python-exercises/two-number-calculator/two-number-calculator.py
+++ python-exercises/two-number-calculator/two-number-calculator.py
@@ -1,3 +1,15 @@
+def multiply(n1, n2) :
+    return n1 * n2
+
+def divide(n1, n2) :
+    return n1 / n2
+
+def add(n1, n2) :
+    return n1 + n2
+
+def subtract(n1, n2) :
+    return n1 - n2
+
 print('CALCULATOR:\n')
 print('(*) Multiplication')
 print('(/) Division')
@@ -6,16 +18,16 @@ print('(-) Subtraction')
 
 operation = input('Enter the symbol corresponding to the desired mathematical operation: ')
 
-number_1 = float(input('Enter the first number: '))
-number_2 = float(input('Enter the second number: '))
+number1 = float(input('Enter the first number: '))
+number2 = float(input('Enter the second number: '))
 
 if operation == '*':
-    print('The multiplication of %.2f and %.2f is %.2f.' % (number_1, number_2, number_1 * number_2))
+    print('The multiplication of %.2f and %.2f is %.2f.' % (number1, number2, multiply(number1, number2)))
 elif operation == '/':
-    print('The division of %.2f and %.2f is %.2f.' % (number_1, number_2, number_1 / number_2))
+    print('The division of %.2f and %.2f is %.2f.' % (number1, number2, divide(number1, number2)))
 elif operation == '+':
-    print('The addition of %.2f and %.2f is %.2f.' % (number_1, number_2, number_1 + number_2))
+    print('The addition of %.2f and %.2f is %.2f.' % (number1, number2, add(number1, number2)))
 elif operation == '-':
-    print('The subtraction of %.2f and %.2f is %.2f.' % (number_1, number_2, number_1 - number_2))
+    print('The subtraction of %.2f and %.2f is %.2f.' % (number1, number2, subtract(number1, number2)))
 else:
     print('The operation you chose is invalid.')
