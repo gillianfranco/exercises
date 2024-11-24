f906acdb8ab139ab823a5aafee7c3e4caa5c988f
Gillian Melo Franco <110102356+gillianfranco@users.noreply.github.com>
2024-11-24 14:25:39 -0300

    Update applying-discount.py

--- python-exercises/applying-discount/applying-discount.py
+++ python-exercises/applying-discount/applying-discount.py
@@ -1,8 +1,6 @@
+apply_discount = lambda price, discount: price - (price * (discount / 100))
+
 price = float(input('Enter the price of the product: '))
-percentage = float(input('Enter a discount percentage: '))
+discount = float(input('Enter a discount percentage: '))
 
-discount = price * (percentage / 100)
-
-calculation = float(price - discount)
-
-print('The initial price of the product is %.2f, with a discount of %.2f%%, the final price is %.2f.' % (price, percentage, calculation))
+print('\nPrice with discount: %.2f' % apply_discount(price, discount))
